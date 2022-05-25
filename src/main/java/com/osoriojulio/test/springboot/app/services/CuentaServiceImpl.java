197f978c776edca89f8374018ae2d5ee166ad7ea
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-05-25 14:21:17 -0500

    implementando la clase de servicio

---
+++ src/main/java/com/osoriojulio/test/springboot/app/services/CuentaServiceImpl.java
@@ -0,0 +1,51 @@
+package com.osoriojulio.test.springboot.app.services;
+
+import com.osoriojulio.test.springboot.app.models.Banco;
+import com.osoriojulio.test.springboot.app.models.Cuenta;
+import com.osoriojulio.test.springboot.app.repositories.BancoRepository;
+import com.osoriojulio.test.springboot.app.repositories.CuentaRepository;
+
+import java.math.BigDecimal;
+
+public class CuentaServiceImpl implements CuentaService{
+    private CuentaRepository cuentaRepository;
+    private BancoRepository bancoRepository;
+
+    public CuentaServiceImpl(CuentaRepository cuentaRepository, BancoRepository bancoRepository) {
+        this.cuentaRepository = cuentaRepository;
+        this.bancoRepository = bancoRepository;
+    }
+
+    @Override
+    public Cuenta findById(Long id) {
+        return cuentaRepository.findById(id);
+    }
+
+    @Override
+    public int revisarTotalTransferencias(Long bancoId) {
+        Banco banco = bancoRepository.findById(bancoId);
+        return banco.getTotalTransferencias();
+    }
+
+    @Override
+    public BigDecimal revisarSalgo(Long cuentaId) {
+        Cuenta cuenta = cuentaRepository.findById(cuentaId);
+        return cuenta.getSaldo();
+    }
+
+    @Override
+    public void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto) {
+        Banco banco = bancoRepository.findById(1L);
+        int totalTransferencias = banco.getTotalTransferencias();
+        banco.setTotalTransferencias(++totalTransferencias);
+        bancoRepository.update(banco);
+
+        Cuenta cuentaOrigen = cuentaRepository.findById(numCuentaOrigen);
+        cuentaOrigen.debito(monto);
+        cuentaRepository.update(cuentaOrigen);
+
+        Cuenta cuentaDestino = cuentaRepository.findById(numCuentaDestino);
+        cuentaDestino.credito(monto);
+        cuentaRepository.update(cuentaDestino);
+    }
+}
