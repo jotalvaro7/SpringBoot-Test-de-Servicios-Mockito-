4aec4c837bb32f0ac3fa228426f8227e717d1bb0
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-05-27 14:31:12 -0500

    Modificando nuestros repositorios con Spring Data JPA parte 2

--- src/main/java/com/osoriojulio/test/springboot/app/repositories/CuentaRepository.java
+++ src/main/java/com/osoriojulio/test/springboot/app/repositories/CuentaRepository.java
@@ -2,10 +2,16 @@ package com.osoriojulio.test.springboot.app.repositories;
 
 import com.osoriojulio.test.springboot.app.models.Cuenta;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CuentaRepository extends JpaRepository<Cuenta, Long> {
+    @Query("select c from Cuenta c where c.persona=?1")
+    Optional<Cuenta> findByPersona(String persona);
+
+
 //    List<Cuenta> findAll();
 //
 //    Cuenta findById(Long id);
