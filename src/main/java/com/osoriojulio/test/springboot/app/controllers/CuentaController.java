23f9a5bb6d56de3155d1657db5c0428ef588551d
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-05-31 08:24:33 -0500

    creando controller, anotacion transaccional a los metodos

---
+++ src/main/java/com/osoriojulio/test/springboot/app/controllers/CuentaController.java
@@ -0,0 +1,41 @@
+package com.osoriojulio.test.springboot.app.controllers;
+
+import static org.springframework.http.HttpStatus.*;
+
+import com.osoriojulio.test.springboot.app.models.Cuenta;
+import com.osoriojulio.test.springboot.app.models.TransaccionDto;
+import com.osoriojulio.test.springboot.app.services.CuentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/cuentas")
+public class CuentaController {
+
+    @Autowired
+    private CuentaService cuentaService;
+
+    @GetMapping("/{id}")
+    @ResponseStatus(OK)
+    public Cuenta detalle(@PathVariable Long id){
+        return cuentaService.findById(id);
+    }
+
+    @PostMapping("/transferir")
+    public ResponseEntity<?> transferir(@RequestBody TransaccionDto dto){
+        cuentaService.transferir(dto.getCuentaOrigenId(), dto.getCuentaDestinoId(), dto.getMonto(), dto.getBancoId());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", OK);
+        response.put("mensaje", "Transferencia realizada con exito");
+        response.put("transaccion", dto);
+
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/com/osoriojulio/test/springboot/app/services/CuentaServiceImpl.java
+++ src/main/java/com/osoriojulio/test/springboot/app/services/CuentaServiceImpl.java
@@ -5,6 +5,7 @@ import com.osoriojulio.test.springboot.app.models.Cuenta;
 import com.osoriojulio.test.springboot.app.repositories.BancoRepository;
 import com.osoriojulio.test.springboot.app.repositories.CuentaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
@@ -19,23 +20,27 @@ public class CuentaServiceImpl implements CuentaService{
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Cuenta findById(Long id) {
         return cuentaRepository.findById(id).orElseThrow();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public int revisarTotalTransferencias(Long bancoId) {
         Banco banco = bancoRepository.findById(bancoId).orElseThrow();
         return banco.getTotalTransferencias();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public BigDecimal revisarSalgo(Long cuentaId) {
         Cuenta cuenta = cuentaRepository.findById(cuentaId).orElseThrow();
         return cuenta.getSaldo();
     }
 
     @Override
+    @Transactional
     public void transferir(Long numCuentaOrigen, Long numCuentaDestino, BigDecimal monto,
                            Long bancoId) {
 
