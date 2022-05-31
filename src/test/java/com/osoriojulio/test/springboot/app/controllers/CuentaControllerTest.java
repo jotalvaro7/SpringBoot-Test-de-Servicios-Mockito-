ab97509cc3468de0d859bc3a69c4b39dba23d67d
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-05-31 11:15:47 -0500

    Escribiendo pruebas unitarias para el controlador con @WebMvcTest y MockMvc

---
+++ src/test/java/com/osoriojulio/test/springboot/app/controllers/CuentaControllerTest.java
@@ -0,0 +1,42 @@
+package com.osoriojulio.test.springboot.app.controllers;
+
+import com.osoriojulio.test.springboot.app.services.CuentaService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.osoriojulio.test.springboot.app.Datos.crearCuenta001;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(CuentaController.class)
+class CuentaControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CuentaService cuentaService;
+
+    @Test
+    void detalle() throws Exception {
+        //Given
+        when(cuentaService.findById(1L)).thenReturn(crearCuenta001().orElseThrow());
+
+        //When
+        mockMvc.perform(get("/api/cuentas/1")
+                        .contentType(MediaType.APPLICATION_JSON))
+                //then
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.persona").value("Andres"))
+                .andExpect(jsonPath("$.saldo").value("1000"));
+
+        verify(cuentaService).findById(1L);
+    }
+}
