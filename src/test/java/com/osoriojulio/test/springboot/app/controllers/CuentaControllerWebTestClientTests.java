7c2d3b89b83eeff7c2a015cfd5135ae8b78b3ee1
Julio Osorio <s_integraciones.cetad@epfac.edu.co>
2022-06-01 13:21:10 -0500

    Configuración y escribiendo las primeras pruebas de integración con WebClient

---
+++ src/test/java/com/osoriojulio/test/springboot/app/controllers/CuentaControllerWebTestClientTests.java
@@ -0,0 +1,64 @@
+package com.osoriojulio.test.springboot.app.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.osoriojulio.test.springboot.app.models.TransaccionDto;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.OK;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+class CuentaControllerWebTestClientTests {
+
+    private ObjectMapper objectMapper;
+    @Autowired
+    private WebTestClient client;
+
+    @BeforeEach
+    void setUp() {
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void transferirTest() throws JsonProcessingException {
+        TransaccionDto dto = new TransaccionDto();
+        dto.setCuentaDestinoId(1L);
+        dto.setCuentaOrigenId(2L);
+        dto.setBancoId(1L);
+        dto.setMonto(new BigDecimal("100"));
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("date", LocalDate.now().toString());
+        response.put("status", OK);
+        response.put("mensaje", "Transferencia realizada con exito");
+        response.put("transaccion", dto);
+
+        client.post().uri("http://localhost:8080/api/cuentas/transferir")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(dto)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody()
+                .jsonPath("$.mensaje").isNotEmpty()
+                .jsonPath("$.mensaje").value(is("Transferencia realizada con exito"))
+                .jsonPath("$.mensaje").value(valor -> {
+                    assertEquals("Transferencia realizada con exito", valor);
+                })
+                .jsonPath("$.transaccion.cuentaOrigenId").isEqualTo(dto.getCuentaOrigenId())
+                .jsonPath("$.date").isEqualTo(LocalDate.now().toString())
+                .json(objectMapper.writeValueAsString(response));
+    }
+}
